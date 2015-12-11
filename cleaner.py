23ce550513cc1d5cc0a1624e72030e85bd9d215c
Dmitrii Suchkov <dsuchkov@codio.com>
2015-12-11 13:45:49 +0000

    cleanup logic

--- cleaner.py
+++ cleaner.py
@@ -3,24 +3,54 @@ __author__ = 'dima'
 import httplib
 from base64 import b64encode
 import time
+from pyhocon import ConfigFactory
+from urlparse import urlparse
+import json
 
-timestamp = int(time.time())
-#2592000
-print timestamp*1000
+def not_exclude(value, excludes):
+    for val in excludes:
+        if val in value:
+            return False
+    return True
 
-userAndPass = b64encode(b"maven:qOwTInzY_4ocuwy0ph_GBhKO0").decode("ascii")
-headers = { 'Authorization' : 'Basic %s' %  userAndPass }
+def clean_path(conn, path, header):
+    conn.request("DELETE", path, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    print data
 
-url = "artifactory.int.codio.com"
+def outdated(conn, url, headers, repo, interval, exclude):      
+    timestamp = int(time.time())
+    date = (timestamp - interval)  * 1000
 
-conn = httplib.HTTPConnection(url, 8081)
+    conn.request("GET", url + "/api/search/usage?notUsedSince=" + str(date) + "&repos=" + repo, headers=headers)
 
-conn.request("GET", "/artifactory/api/search/usage?notUsedSince=1449670852990&repos=component-builds", headers=headers)
+    res = conn.getresponse()
 
-res = conn.getresponse()
+    data = res.read()
 
-print res.status, res.reason
+    json_object = json.loads(data)
+    
+    if 'results' in json_object:    
+        for value in json_object["results"]:
+            if not_exclude(value["uri"], exclude):
+                updated = value["uri"].replace("api/storage/", "")
+                substr = "/artifactory/"
+                pos = updated.index(substr)
+                path = updated[pos:len(updated)]
+                print path
+                clean_path(conn, path, headers)
 
-data = res.read()
 
-print data
+config = ConfigFactory.parse_file("cleaner.conf")
+
+artifactory_url = urlparse(config.get("artifactory.url"))
+port = artifactory_url.port if artifactory_url.port else 80 if artifactory_url.scheme == 'http' else 443
+
+conn = httplib.HTTPConnection(artifactory_url.hostname, port) if artifactory_url.scheme == 'http' else httplib.HTTPSConnection(artifactory_url.hostname, port)
+
+user_pass = b64encode(config.get("artifactory.auth")).decode("ascii")
+headers = { 'Authorization' : 'Basic %s' %  user_pass }
+
+for value in config.get("repos"):
+    outdated(conn, artifactory_url.path, headers, value.get('name'), value.get_int('interval'), config.get("exclude"))
